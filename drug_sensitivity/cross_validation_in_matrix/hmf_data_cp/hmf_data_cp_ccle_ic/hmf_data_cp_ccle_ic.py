c4f14522c5f5748ff9b00eee1c736137def208de
ThomasBrouwer <tab43@cam.ac.uk>
2016-11-15 13:09:15 +0000

    Added cross-validation experiments for tensor decomposition (CP) model using HMF, on both drug sensitivity and methylation datasets. For methylation data especially we see a huge decrease in predictive performance.

---
+++ drug_sensitivity/cross_validation_in_matrix/hmf_data_cp/hmf_data_cp_ccle_ic/hmf_data_cp_ccle_ic.py
@@ -0,0 +1,101 @@
+"""
+Run the cross validation for HMF (datasets, using CP) on the drug sensitivity
+datasets.
+"""
+
+project_location = "/home/tab43/Documents/Projects/libraries/"
+import sys
+sys.path.append(project_location)
+from HMF.code.cross_validation.cross_validation_hmf import CrossValidation
+from HMF.drug_sensitivity.load_dataset import load_data_without_empty, load_data_filter
+
+import numpy, random
+
+
+''' Load datasets '''
+location = project_location+"HMF/drug_sensitivity/data/overlap/"
+location_data =                 location+"data_row_01/"
+location_features_drugs =       location+"features_drugs/"
+location_features_cell_lines =  location+"features_cell_lines/"
+location_kernels =              location+"kernels_features/"
+
+R_ccle_ic,  M_ccle_ic, cell_lines, drugs  = load_data_without_empty(location_data+"ccle_ic50_row_01.txt")
+R_ctrp,     M_ctrp                        = load_data_filter(location_data+"ctrp_ec50_row_01.txt",cell_lines,drugs)
+R_gdsc,     M_gdsc                        = load_data_filter(location_data+"gdsc_ic50_row_01.txt",cell_lines,drugs)
+R_ccle_ec,  M_ccle_ec                     = load_data_filter(location_data+"ccle_ec50_row_01.txt",cell_lines,drugs)
+
+
+''' Settings HMF '''
+iterations, burn_in, thinning = 200, 180, 2
+no_folds = 10
+
+settings = {
+    'priorF'  : 'exponential',
+    'orderG'  : 'normal',
+    'priorSn' : 'normal',
+    'priorSm' : 'normal',
+    'orderF'  : 'columns',
+    'orderG'  : 'rows',
+    'orderSn' : 'rows',
+    'orderSm' : 'rows',
+    'ARD'     : True,
+    'tensor_decomposition' : True,
+}
+hyperparameters = {
+    'alphatau' : 1.,
+    'betatau'  : 1.,
+    'alpha0'   : 0.001,
+    'beta0'    : 0.001,
+    'lambdaF'  : 0.1,
+    'lambdaG'  : 0.1,
+    'lambdaSn' : 0.1,
+    'lambdaSm' : 0.1,
+}
+init = {
+    'F'       : 'kmeans',
+    'Sn'      : 'least',
+    'Sm'      : 'least',
+    'G'       : 'least',
+    'lambdat' : 'exp',
+    'tau'     : 'exp'
+}
+
+K = {'Cell_lines':20, 'Drugs':20}
+alpha_n = [1., 1., 1., 1.] # GDSC, CTRP, CCLE IC, CCLE EC
+alpha_m = []
+
+
+''' Assemble R, C, D. '''
+R = [(R_gdsc,    M_gdsc,    'Cell_lines', 'Drugs', alpha_n[0]), 
+     (R_ctrp,    M_ctrp,    'Cell_lines', 'Drugs', alpha_n[1]), 
+     (R_ccle_ic, M_ccle_ic, 'Cell_lines', 'Drugs', alpha_n[2]),
+     (R_ccle_ec, M_ccle_ec, 'Cell_lines', 'Drugs', alpha_n[3])]
+C, D = [], []
+
+main_dataset = 'R'
+index_main = 2 # CCLE IC
+file_performance = 'results_2020.txt'
+
+
+''' Run the cross-validation framework '''
+random.seed(0)
+numpy.random.seed(0)
+crossval = CrossValidation(
+    folds=no_folds,
+    main_dataset=main_dataset,
+    index_main=index_main,
+    R=R,
+    C=C,
+    D=D,
+    K=K,
+    settings=settings,
+    hyperparameters=hyperparameters,
+    init=init,
+    file_performance=file_performance
+)
+crossval.run(iterations=iterations,burn_in=burn_in,thinning=thinning)
+
+
+"""
+
+"""
