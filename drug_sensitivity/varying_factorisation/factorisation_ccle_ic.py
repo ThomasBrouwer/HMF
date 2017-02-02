c84b804ee117008850686c2bb91079db25580c25
ThomasBrouwer <tab43@cam.ac.uk>
2017-02-02 17:14:09 +0000

    Modified varying factorisation drug sensitivity experiments to do Kmeans init for F (have to make sure that if the main dataset is not R, and 2/3 others are R, we still have a dataset with a datapoint for each drug (column). Not as straightforward as it seems.

--- drug_sensitivity/varying_factorisation/factorisation_ccle_ic.py
+++ drug_sensitivity/varying_factorisation/factorisation_ccle_ic.py
@@ -11,6 +11,7 @@ sys.path.append(project_location)
 from HMF.code.cross_validation.cross_validation_hmf import CrossValidation
 from HMF.drug_sensitivity.load_dataset import load_data_without_empty, load_data_filter
 
+import numpy
 
 ''' Load datasets '''
 location = project_location+"HMF/drug_sensitivity/data/overlap/"
@@ -26,7 +27,7 @@ R_ccle_ec,  M_ccle_ec                    = load_data_filter(location_data+"ccle_
 
 
 ''' Settings HMF '''
-iterations, burn_in, thinning = 500, 400, 2
+iterations, burn_in, thinning = 200, 150, 2 # 500, 400, 2
 no_folds = 10
 
 hyperparameters = {
@@ -51,7 +52,7 @@ settings = {
     'ARD'     : True
 }
 init = {
-    'F'       : 'random', #'kmeans' gives error when CCLE IC,EC are both 'D' (no fully observed column in GDSC or CTRP)
+    'F'       : 'kmeans',
     'Sn'      : 'random',
     'Sm'      : 'random',
     'G'       : 'random',
@@ -67,16 +68,16 @@ file_performance = 'results_ccle_ic.txt'
 
 ''' The different factorisations, and a method to construct the matrices. '''
 values_factorisation = [
-    { 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
-    { 'GDSC': 'D', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
-    { 'GDSC': 'R', 'CTRP': 'D', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
-    { 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'D', 'CCLE_EC': 'R' },
-    { 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'D' },
-    { 'GDSC': 'D', 'CTRP': 'D', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
-    { 'GDSC': 'D', 'CTRP': 'R', 'CCLE_IC': 'D', 'CCLE_EC': 'R' },
-    { 'GDSC': 'D', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'D' },
-    { 'GDSC': 'R', 'CTRP': 'D', 'CCLE_IC': 'D', 'CCLE_EC': 'R' },
-    { 'GDSC': 'R', 'CTRP': 'D', 'CCLE_IC': 'R', 'CCLE_EC': 'D' },
+    #{ 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'D', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'R', 'CTRP': 'D', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'D', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'D' },
+    #{ 'GDSC': 'D', 'CTRP': 'D', 'CCLE_IC': 'R', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'D', 'CTRP': 'R', 'CCLE_IC': 'D', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'D', 'CTRP': 'R', 'CCLE_IC': 'R', 'CCLE_EC': 'D' },
+    #{ 'GDSC': 'R', 'CTRP': 'D', 'CCLE_IC': 'D', 'CCLE_EC': 'R' },
+    #{ 'GDSC': 'R', 'CTRP': 'D', 'CCLE_IC': 'R', 'CCLE_EC': 'D' },
     { 'GDSC': 'R', 'CTRP': 'R', 'CCLE_IC': 'D', 'CCLE_EC': 'D' },
     { 'GDSC': 'D', 'CTRP': 'D', 'CCLE_IC': 'D', 'CCLE_EC': 'D' },
 ]
@@ -86,28 +87,60 @@ def construct_RCD(factorisation, dataset_to_predict):
         'D', construct the (R,C,D) lists. For the Y entry, use M_Y as the mask. 
         Note that the Y entry always has index 0. 
         Return a tuple (index_main,R,C,D), where index_main gives the index of
-        dataset_to_predict in either R or D. '''
+        dataset_to_predict in either R or D. 
+        If the :dataset_to_predict is not an R dataset, and there are other R
+        datasets, we have to remove columns with no observed datapoints in one
+        of the R datasets (to make sure Kmeans init works). '''
     R, C, D = [], [], []
     
+    # If the main dataset is not R, we have to filter some columns
+    if factorisation[dataset_to_predict] == 'R':    
+        R_gdsc_filtered, M_gdsc_filtered = (numpy.copy(R_gdsc), numpy.copy(M_gdsc))
+        R_ctrp_filtered, M_ctrp_filtered = (numpy.copy(R_ctrp), numpy.copy(M_ctrp))
+        R_ccle_ic_filtered, M_ccle_ic_filtered = (numpy.copy(R_ccle_ic), numpy.copy(M_ccle_ic))
+        R_ccle_ec_filtered, M_ccle_ec_filtered = (numpy.copy(R_ccle_ec), numpy.copy(M_ccle_ec))
+    else:
+        # For each of the R dataset, compute which columns to keep
+        columns_gdsc    = (M_gdsc.sum(axis=0) > 0)    if factorisation['GDSC'] == 'R'    else []
+        columns_ctrp    = (M_ctrp.sum(axis=0) > 0)    if factorisation['CTRP'] == 'R'    else []
+        columns_ccle_ic = (M_ccle_ic.sum(axis=0) > 0) if factorisation['CCLE_IC'] == 'R' else []
+        columns_ccle_ec = (M_ccle_ec.sum(axis=0) > 0) if factorisation['CCLE_EC'] == 'R' else []
+        
+        # Keep the columns of the dataset with the most non-zero columns
+        n_cols_gdsc, n_cols_ctrp, n_cols_ccle_ic, n_cols_ccle_ec = sum(columns_gdsc), sum(columns_ctrp), sum(columns_ccle_ic), sum(columns_ccle_ec)
+        max_n = max(n_cols_gdsc, n_cols_ctrp, n_cols_ccle_ic, n_cols_ccle_ec)
+        cols_to_keep = columns_gdsc if n_cols_gdsc == max_n else columns_ctrp if n_cols_ctrp == max_n else columns_ccle_ic if n_cols_ccle_ic == max_n else columns_ccle_ec
+        
+        # For the R datasets, select those columns
+        R_gdsc_filtered, M_gdsc_filtered = (numpy.copy(R_gdsc), numpy.copy(M_gdsc)) if factorisation['GDSC'] == 'D' \
+            else (R_gdsc[:,cols_to_keep], M_gdsc[:,cols_to_keep])
+        R_ctrp_filtered, M_ctrp_filtered = (numpy.copy(R_ctrp), numpy.copy(M_ctrp)) if factorisation['CTRP'] == 'D' \
+            else (R_ctrp[:,cols_to_keep], M_ctrp[:,cols_to_keep])
+        R_ccle_ic_filtered, M_ccle_ic_filtered = (numpy.copy(R_ccle_ic), numpy.copy(M_ccle_ic)) if factorisation['CCLE_IC'] == 'D' \
+            else (R_ccle_ic[:,cols_to_keep], M_ccle_ic[:,cols_to_keep])
+        R_ccle_ec_filtered, M_ccle_ec_filtered = (numpy.copy(R_ccle_ec), numpy.copy(M_ccle_ec)) if factorisation['CCLE_EC'] == 'D' \
+            else (R_ccle_ec[:,cols_to_keep], M_ccle_ec[:,cols_to_keep])
+    
+    # Construct R, C, D lists    
     if factorisation['GDSC'] == 'R':
-        R.append((R_gdsc, M_gdsc, 'Cell_lines', 'Drugs', alpha[0]))
+        R.append((R_gdsc_filtered, M_gdsc_filtered, 'Cell_lines', 'Drugs', alpha[0]))
     else: # factorisation['GDSC'] == 'D'
-        D.append((R_gdsc, M_gdsc, 'Cell_lines', alpha[0]))
+        D.append((R_gdsc_filtered, M_gdsc_filtered, 'Cell_lines', alpha[0]))
         
     if factorisation['CTRP'] == 'R':
-        R.append((R_ctrp, M_ctrp, 'Cell_lines', 'Drugs', alpha[1]))
+        R.append((R_ctrp_filtered, M_ctrp_filtered, 'Cell_lines', 'Drugs', alpha[1]))
     else: # factorisation['CTRP'] == 'D'
-        D.append((R_ctrp, M_ctrp, 'Cell_lines', alpha[1]))
+        D.append((R_ctrp_filtered, M_ctrp_filtered, 'Cell_lines', alpha[1]))
         
     if factorisation['CCLE_EC'] == 'R':
-        R.append((R_ccle_ec, M_ccle_ec, 'Cell_lines', 'Drugs', alpha[3]))
+        R.append((R_ccle_ec_filtered, M_ccle_ec_filtered, 'Cell_lines', 'Drugs', alpha[3]))
     else: # factorisation['CCLE_EC'] == 'D'
-        D.append((R_ccle_ec, M_ccle_ec, 'Cell_lines', alpha[3]))
+        D.append((R_ccle_ec_filtered, M_ccle_ec_filtered, 'Cell_lines', alpha[3]))
         
     if factorisation['CCLE_IC'] == 'R':
-        R.append((R_ccle_ic, M_ccle_ic, 'Cell_lines', 'Drugs', alpha[2]))
+        R.append((R_ccle_ic_filtered, M_ccle_ic_filtered, 'Cell_lines', 'Drugs', alpha[2]))
     else: # factorisation['CCLE_IC'] == 'D'
-        D.append((R_ccle_ic, M_ccle_ic, 'Cell_lines', alpha[2]))
+        D.append((R_ccle_ic_filtered, M_ccle_ic_filtered, 'Cell_lines', alpha[2]))
         
     index_main = len(R) - 1 if factorisation[dataset_to_predict] == 'R' else len(D) - 1
     return (index_main, R, C, D)
--- drug_sensitivity/varying_factorisation/factorisation_gdsc.py
+++ drug_sensitivity/varying_factorisation/factorisation_gdsc.py
@@ -28,7 +28,7 @@ R_ccle_ec,  M_ccle_ec                    = load_data_filter(location_data+"ccle_
 
 
 ''' Settings HMF '''
-iterations, burn_in, thinning = 500, 400, 2
+iterations, burn_in, thinning = 200, 150, 2 #500, 400, 2
 no_folds = 10
 
 hyperparameters = {
@@ -53,7 +53,7 @@ settings = {
     'ARD'     : True
 }
 init = {
-    'F'       : 'random', #'kmeans' gives error
+    'F'       : 'kmeans',
     'Sn'      : 'random',
     'Sm'      : 'random',
     'G'       : 'random',
@@ -89,29 +89,38 @@ def construct_RCD(factorisation, dataset_to_predict):
         Note that the Y entry always has index 0. 
         Return a tuple (index_main,R,C,D), where index_main gives the index of
         dataset_to_predict in either R or D.
-        If we have any R datasets, remove any columns (drugs) with no observed 
-        datapoint in any of the datasets (otherwise HMF throws an error). '''
+        If the :dataset_to_predict is not an R dataset, and there are other R
+        datasets, we have to remove columns with no observed datapoints in one
+        of the R datasets (to make sure Kmeans init works). '''
     R, C, D = [], [], []
+    
+    # If the main dataset is not R, we have to filter some columns
+    if factorisation[dataset_to_predict] == 'R':    
+        R_gdsc_filtered, M_gdsc_filtered = (numpy.copy(R_gdsc), numpy.copy(M_gdsc))
+        R_ctrp_filtered, M_ctrp_filtered = (numpy.copy(R_ctrp), numpy.copy(M_ctrp))
+        R_ccle_ic_filtered, M_ccle_ic_filtered = (numpy.copy(R_ccle_ic), numpy.copy(M_ccle_ic))
+        R_ccle_ec_filtered, M_ccle_ec_filtered = (numpy.copy(R_ccle_ec), numpy.copy(M_ccle_ec))
+    else:
+        # For each of the R dataset, compute which columns to keep
+        columns_gdsc    = (M_gdsc.sum(axis=0) > 0)    if factorisation['GDSC'] == 'R'    else []
+        columns_ctrp    = (M_ctrp.sum(axis=0) > 0)    if factorisation['CTRP'] == 'R'    else []
+        columns_ccle_ic = (M_ccle_ic.sum(axis=0) > 0) if factorisation['CCLE_IC'] == 'R' else []
+        columns_ccle_ec = (M_ccle_ec.sum(axis=0) > 0) if factorisation['CCLE_EC'] == 'R' else []
         
-    # Combine the masks of any R factorisation datasets into one (logical or).
-    Ms = []
-    if factorisation['GDSC'] == 'R': Ms.append(M_gdsc)
-    if factorisation['CTRP'] == 'R': Ms.append(M_ctrp)
-    if factorisation['CCLE_IC'] == 'R': Ms.append(M_ccle_ic)
-    if factorisation['CCLE_EC'] == 'R': Ms.append(M_ccle_ec)
-    M_combined = numpy.zeros(M_gdsc.shape)
-    for M in Ms: 
-        M_combined = numpy.logical_or(M_combined, M)
+        # Keep the columns of the dataset with the most non-zero columns
+        n_cols_gdsc, n_cols_ctrp, n_cols_ccle_ic, n_cols_ccle_ec = sum(columns_gdsc), sum(columns_ctrp), sum(columns_ccle_ic), sum(columns_ccle_ec)
+        max_n = max(n_cols_gdsc, n_cols_ctrp, n_cols_ccle_ic, n_cols_ccle_ec)
+        cols_to_keep = columns_gdsc if n_cols_gdsc == max_n else columns_ctrp if n_cols_ctrp == max_n else columns_ccle_ic if n_cols_ccle_ic == max_n else columns_ccle_ec
         
-    # Filter the columns of all R factorisation datasets where we have no observed datapoint in any of the R factorisations.
-    R_gdsc_filtered, M_gdsc_filtered = (numpy.copy(R_gdsc), numpy.copy(M_gdsc)) if factorisation['GDSC'] == 'D' \
-        else (R_gdsc[:,M_combined.sum(axis=0) > 0], M_gdsc[:,M_combined.sum(axis=0) > 0])
-    R_ctrp_filtered, M_ctrp_filtered = (numpy.copy(R_ctrp), numpy.copy(M_ctrp)) if factorisation['CTRP'] == 'D' \
-        else (R_ctrp[:,M_combined.sum(axis=0) > 0], M_ctrp[:,M_combined.sum(axis=0) > 0])
-    R_ccle_ic_filtered, M_ccle_ic_filtered = (numpy.copy(R_ccle_ic), numpy.copy(M_ccle_ic)) if factorisation['CCLE_IC'] == 'D' \
-        else (R_ccle_ic[:,M_combined.sum(axis=0) > 0], M_ccle_ic[:,M_combined.sum(axis=0) > 0])
-    R_ccle_ec_filtered, M_ccle_ec_filtered = (numpy.copy(R_ccle_ec), numpy.copy(M_ccle_ec)) if factorisation['CCLE_EC'] == 'D' \
-        else (R_ccle_ec[:,M_combined.sum(axis=0) > 0], M_ccle_ec[:,M_combined.sum(axis=0) > 0])
+        # For the R datasets, select those columns
+        R_gdsc_filtered, M_gdsc_filtered = (numpy.copy(R_gdsc), numpy.copy(M_gdsc)) if factorisation['GDSC'] == 'D' \
+            else (R_gdsc[:,cols_to_keep], M_gdsc[:,cols_to_keep])
+        R_ctrp_filtered, M_ctrp_filtered = (numpy.copy(R_ctrp), numpy.copy(M_ctrp)) if factorisation['CTRP'] == 'D' \
+            else (R_ctrp[:,cols_to_keep], M_ctrp[:,cols_to_keep])
+        R_ccle_ic_filtered, M_ccle_ic_filtered = (numpy.copy(R_ccle_ic), numpy.copy(M_ccle_ic)) if factorisation['CCLE_IC'] == 'D' \
+            else (R_ccle_ic[:,cols_to_keep], M_ccle_ic[:,cols_to_keep])
+        R_ccle_ec_filtered, M_ccle_ec_filtered = (numpy.copy(R_ccle_ec), numpy.copy(M_ccle_ec)) if factorisation['CCLE_EC'] == 'D' \
+            else (R_ccle_ec[:,cols_to_keep], M_ccle_ec[:,cols_to_keep])
     
     # Construct R, C, D lists
     if factorisation['CTRP'] == 'R':
