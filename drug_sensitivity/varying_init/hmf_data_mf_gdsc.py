0521a175b4b7b52983946dc11fe5cd94c6e17ff5
ThomasBrouwer <tab43@cam.ac.uk>
2017-08-13 23:36:02 +0200

    Redid initialisation experiments to average across 10 runs. Now running.

---
+++ drug_sensitivity/varying_init/hmf_data_mf_gdsc.py
@@ -0,0 +1,157 @@
+"""
+Measure the convergence of the drug sensitivity datasets for HMF (datasets,
+using MTF). We run ten repeats and average across them.
+"""
+
+project_location = "/Users/thomasbrouwer/Documents/Projects/libraries/"
+import sys
+sys.path.append(project_location)
+from HMF.code.models.hmf_Gibbs import HMF_Gibbs
+from HMF.drug_sensitivity.load_dataset import load_data_without_empty, load_data_filter
+
+import numpy
+
+
+''' Load datasets '''
+location = project_location+"HMF/drug_sensitivity/data/overlap/"
+location_data =                 location+"data_row_01/"
+location_features_drugs =       location+"features_drugs/"
+location_features_cell_lines =  location+"features_cell_lines/"
+location_kernels =              location+"kernels_features/"
+
+R_gdsc,     M_gdsc,   cell_lines, drugs   = load_data_without_empty(location_data+"gdsc_ic50_row_01.txt")
+R_ccle_ec,  M_ccle_ec                     = load_data_filter(location_data+"ccle_ec50_row_01.txt",cell_lines,drugs)
+R_ctrp,     M_ctrp                        = load_data_filter(location_data+"ctrp_ec50_row_01.txt",cell_lines,drugs)
+R_ccle_ic,  M_ccle_ic                     = load_data_filter(location_data+"ccle_ic50_row_01.txt",cell_lines,drugs)
+
+
+''' Settings HMF '''
+iterations = 500
+n_repeats = 10
+hyperparameters = {
+    'alphatau' : 1.,
+    'betatau'  : 1.,
+    'alpha0'   : 0.001,
+    'beta0'    : 0.001,
+    'lambdaF'  : 0.1,
+    'lambdaG'  : 0.1,
+    'lambdaSn' : 0.1,
+    'lambdaSm' : 0.1,
+}
+settings = {
+    'priorF'  : 'exponential',
+    'priorG'  : 'normal',
+    'priorSn' : 'normal',
+    'priorSm' : 'normal',
+    'orderF'  : 'columns',
+    'orderG'  : 'rows',
+    'orderSn' : 'rows',
+    'orderSm' : 'rows',
+    'ARD'     : True
+}
+
+alpha_l = [1., 1., 1., 1.] # GDSC, CTRP, CCLE IC, CCLE EC
+K = {'Cell_lines':10, 'Drugs':10}
+
+values_init = [
+    # All expectation
+    {
+        'F'       : 'exp',
+        'Sn'      : 'exp',
+        'Sm'      : 'exp',
+        'G'       : 'exp',
+        'lambdat' : 'exp',
+        'tau'     : 'exp'
+    },
+    # All random
+    {
+        'F'       : 'random',
+        'Sn'      : 'random',
+        'Sm'      : 'random',
+        'G'       : 'random',
+        'lambdat' : 'exp',
+        'tau'     : 'random'
+    },
+    # Kmeans and expectation
+    {
+        'F'       : 'kmeans',
+        'Sn'      : 'exp',
+        'Sm'      : 'exp',
+        'G'       : 'exp',
+        'lambdat' : 'exp',
+        'tau'     : 'exp'
+    },
+    # Kmeans and random
+    {
+        'F'       : 'kmeans',
+        'Sn'      : 'random',
+        'Sm'      : 'random',
+        'G'       : 'random',
+        'lambdat' : 'exp',
+        'tau'     : 'exp'
+    },
+    # Exp and least squares
+    {
+        'F'       : 'exp',
+        'Sn'      : 'least',
+        'Sm'      : 'least',
+        'G'       : 'least',
+        'lambdat' : 'exp',
+        'tau'     : 'exp'
+    },
+    # Random and least squares
+    {
+        'F'       : 'random',
+        'Sn'      : 'least',
+        'Sm'      : 'least',
+        'G'       : 'least',
+        'lambdat' : 'exp',
+        'tau'     : 'exp'
+    },
+    # Kmeans and least squares
+    {
+        'F'       : 'kmeans',
+        'Sn'      : 'least',
+        'Sm'      : 'least',
+        'G'       : 'least',
+        'lambdat' : 'exp',
+        'tau'     : 'exp'
+    },
+]
+
+
+R, C = [], []
+main_dataset = 'D'
+
+
+
+R, C = [], []
+D = [(R_gdsc,    M_gdsc,    'Cell_lines', alpha_l[0]), 
+     (R_ctrp,    M_ctrp,    'Cell_lines', alpha_l[1]), 
+     (R_ccle_ic, M_ccle_ic, 'Cell_lines', alpha_l[2]),
+     (R_ccle_ec, M_ccle_ec, 'Cell_lines', alpha_l[3])]
+main_dataset = 'D'
+index_main = 0 # GDSC
+file_performances = './results/hmf_data_mf_gdsc_ic.txt'
+    
+
+
+''' Run the methods with different inits and measure the convergence. '''
+all_init_performances = []
+for init in values_init:
+    all_performances = []
+    for n in range(n_repeats):
+        print "Repeat %s of initialisation experiment, with init %s." % (n, init)
+        
+        HMF = HMF_Gibbs(R,C,D,K,settings,hyperparameters)
+        HMF.initialise(init)
+        HMF.run(iterations)
+        all_performances.append(HMF.all_performances_Dl['MSE'][index_main])
+        
+    average_performances = list(numpy.mean(all_performances, axis=0))
+    all_init_performances.append(average_performances)
+        
+    
+''' Store performances in file. '''
+with open(file_performances,'w') as fout:
+    fout.write("%s" % all_init_performances)
---
+++ drug_sensitivity/varying_init/plots/plot.py
@@ -0,0 +1,64 @@
+'''
+Plot the convergence of both methods on the four datasets.
+'''
+
+import matplotlib.pyplot as plt
+
+
+
+''' Load in the performances. '''
+folder_performances = './../results/'
+mf_ccle_ec = eval(open(folder_performances+'hmf_data_mf_ccle_ec.txt','r').readline())
+mf_ccle_ic = eval(open(folder_performances+'hmf_data_mf_ccle_ic.txt','r').readline())
+#mf_ctrp_ec = eval(open(folder_performances+'hmf_data_mf_ctrp_ec.txt','r').readline())
+#mf_gdsc_ic = eval(open(folder_performances+'hmf_data_mf_gdsc_ic.txt','r').readline())
+
+
+''' Plot settings. '''
+iterations = range(len(mf_ccle_ec[0])+1)
+iterations_mf, iterations_mf = 50, 50
+colours = ['#0000ff','#00ff00','#ff0000','#00ffff','#ff00ff','#ffff00','#00007f']#,'#007f00']
+markersize = 1
+
+
+''' Method for plotting. '''
+def plot(name, MSE_min, MSE_max, all_performances, no_iterations):
+    fig = plt.figure(figsize=(3.8,3.0))
+    fig.subplots_adjust(left=0.15, right=0.965, bottom=0.12, top=0.97)
+    plt.xlabel("Iterations", fontsize=12, labelpad=1) #fontsize=8
+    plt.ylabel("MSE", fontsize=12, labelpad=2) #fontsize=8, labelpad=-1
+    plt.yticks(fontsize=8) #fontsize=6
+    plt.xticks(fontsize=8) #fontsize=6
+    
+    plt.plot(iterations[:no_iterations], all_performances[0][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='Exp', c=colours[0], markersize=markersize)
+    plt.plot(iterations[:no_iterations], all_performances[1][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='Random', c=colours[1], markersize=markersize)
+    plt.plot(iterations[:no_iterations], all_performances[2][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='K-means, exp', c=colours[2], markersize=markersize)
+    plt.plot(iterations[:no_iterations], all_performances[3][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='K-means, random', c=colours[3], markersize=markersize)
+    plt.plot(iterations[:no_iterations], all_performances[4][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='Exp, least squares', c=colours[4], markersize=markersize)
+    plt.plot(iterations[:no_iterations], all_performances[5][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='Random, least squares', c=colours[5], markersize=markersize)
+    plt.plot(iterations[:no_iterations], all_performances[6][:no_iterations], 
+             linestyle='-', linewidth=1.2, marker='o', label='K-means, least squares', c=colours[6], markersize=markersize)
+    
+    plt.xlim(0,no_iterations+1)
+    plt.ylim(MSE_min,MSE_max)
+    
+    plt.legend(loc='upper right',fontsize=8)
+    plt.savefig(folder_plots+"%s.png" % name, dpi=600)
+    
+    
+''' Make the plots. '''
+folder_plots = './'
+plot('ccle_ec_mf',  0.055, 0.13,  mf_ccle_ec, iterations_mf)
+plot('ccle_ic_mf',  0.015, 0.07,  mf_ccle_ic, iterations_mf)
+#plot('ctrp_ec_mf',  0.07,  0.085, mf_ctrp_ec, iterations_mf)
+#plot('gdsc_ic_mf',  0.055, 0.07,  mf_gdsc_ic, iterations_mf)
+#plot('ccle_ec_mtf', 0.055, 0.17, mtf_ccle_ec, iterations_mtf)
+#plot('ccle_ic_mtf', 0.025, 0.07, mtf_ccle_ic, iterations_mtf)
+#plot('ctrp_ec_mtf', 0.076, 0.12, mtf_ctrp_ec, iterations_mtf)
+#plot('gdsc_ic_mtf', 0.06,  0.12, mtf_gdsc_ic, iterations_mtf)
